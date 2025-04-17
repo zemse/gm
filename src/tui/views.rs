84f421965c34e65e43266d8fc9e13af0e428e6ba
Soham Zemse <22412996+zemse@users.noreply.github.com>
2025-04-17 12:02:58 +0530

    rm body module

--- src/tui/views.rs
+++ src/tui/views.rs
@@ -1,19 +1,21 @@
 use std::io;
 
-use body::Body;
 use footer::Footer;
+use left::Left;
 use ratatui::{
     layout::{Constraint, Layout},
-    widgets::Widget,
+    widgets::{Block, BorderType, Widget},
     DefaultTerminal,
 };
+use right::Right;
 use title::Title;
 
 use super::{controller::navigation::Navigation, traits::BorderedWidget};
 
-mod body;
 mod components;
 mod footer;
+mod left;
+mod right;
 mod title;
 
 pub struct View<'a> {
@@ -49,13 +51,29 @@ impl Widget for &View<'_> {
             Constraint::Length(1),
         ]);
         let [title_area, body_area, footer_area] = vertical_layout.areas(area);
+        let horizontal_layout =
+            Layout::horizontal([Constraint::Percentage(70), Constraint::Percentage(30)]);
+        let [left_area, right_area] = horizontal_layout.areas(body_area);
 
         Title.render(title_area, buf);
-        Body {
-            eth_price: self.eth_price,
-            navigation: self.navigation,
+        Left {
+            page: self.navigation.current_page(),
+            text_input: self.navigation.text_input.clone(),
+            _marker: std::marker::PhantomData,
         }
-        .render(body_area, buf);
+        .render_with_block(
+            left_area,
+            buf,
+            Block::bordered().border_type(BorderType::Plain),
+        );
+        Right {
+            eth_price: self.eth_price,
+        }
+        .render_with_block(
+            right_area,
+            buf,
+            Block::bordered().border_type(BorderType::Plain),
+        );
         Footer {
             exit: self.exit,
             navigation: self.navigation,
