5fa4eb8608eda70fd52191232b9fd9fd39b9770e
Soham Zemse <22412996+zemse@users.noreply.github.com>
2025-06-21 14:32:49 +0530

    rm Borders and use BorderType directly

--- src/tui/theme.rs
+++ src/tui/theme.rs
@@ -17,6 +17,7 @@ impl std::fmt::Display for ThemeName {
         write!(f, "{:?}", self)
     }
 }
+
 impl ThemeName {
     pub fn from_str(theme_name: &str) -> Self {
         match theme_name {
@@ -28,11 +29,7 @@ impl ThemeName {
         }
     }
 }
-#[derive(Clone)]
-pub enum Borders {
-    Square,
-    Rounded,
-}
+
 #[derive(Clone)]
 pub struct Theme {
     pub text: Color,
@@ -40,8 +37,9 @@ pub struct Theme {
     pub select: Option<Color>,
     pub select_popup: Option<Color>,
     pub error_popup: Option<Color>,
-    pub borders: Borders,
+    pub border_type: BorderType,
 }
+
 impl Default for Theme {
     fn default() -> Self {
         Self {
@@ -50,34 +48,35 @@ impl Default for Theme {
             select: None,
             select_popup: None,
             error_popup: None,
-            borders: Borders::Square,
+            border_type: BorderType::Plain,
         }
     }
 }
+
 impl From<&Theme> for Style {
     fn from(val: &Theme) -> Self {
         Style::default().bg(val.bg).fg(val.text)
     }
 }
+
 impl From<&mut Theme> for Style {
     fn from(val: &mut Theme) -> Self {
         Style::default().bg(val.bg).fg(val.text)
     }
 }
+
 impl From<&Theme> for BorderType {
     fn from(val: &Theme) -> Self {
-        match val.borders {
-            Borders::Square => BorderType::Plain,
-            Borders::Rounded => BorderType::Rounded,
-        }
+        val.border_type
     }
 }
+
 impl Theme {
     pub fn new(theme_name: ThemeName) -> Theme {
         match theme_name {
             ThemeName::Monochrome => Default::default(),
             ThemeName::MonochromeModern => Theme {
-                borders: Borders::Rounded,
+                border_type: BorderType::Rounded,
                 ..Default::default()
             },
             ThemeName::Dark => Theme {
@@ -86,7 +85,7 @@ impl Theme {
                 select: Some(Color::Yellow),
                 select_popup: Some(Color::Blue),
                 error_popup: Some(Color::Red),
-                borders: Borders::Square,
+                border_type: BorderType::Plain,
             },
             ThemeName::DarkModern => Theme {
                 text: Color::White,
@@ -94,14 +93,16 @@ impl Theme {
                 select: Some(Color::Yellow),
                 select_popup: Some(Color::Blue),
                 error_popup: Some(Color::Red),
-                borders: Borders::Rounded,
+                border_type: BorderType::Rounded,
             },
         }
     }
+
     pub fn select(&self) -> Option<Style> {
         self.select
             .map(|select| Into::<Style>::into(self).bg(select))
     }
+
     pub fn select_popup(&self) -> Style {
         if let Some(select_popup) = self.select_popup {
             Into::<Style>::into(self)
@@ -111,6 +112,7 @@ impl Theme {
             Style::default().remove_modifier(Modifier::REVERSED)
         }
     }
+
     pub fn error_popup(&self) -> Theme {
         if let Some(error_popup) = self.error_popup {
             Theme {
