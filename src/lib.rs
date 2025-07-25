edcc94ca157611936c3cfbcb6f366afc23876f3e
Soham Zemse <22412996+zemse@users.noreply.github.com>
2025-07-26 03:37:17 +0530

    add gm_log macro

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,19 @@ pub mod network;
 pub mod tui;
 pub mod utils;
 pub use error::{Error, Result};
+
+#[macro_export]
+macro_rules! gm_log {
+    ($($arg:tt)*) => {{
+        use std::fs::OpenOptions;
+        use std::io::Write;
+
+        let mut file = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open("gm.log")
+            .unwrap();
+
+        writeln!(file, $($arg)*).unwrap();
+    }};
+}
